af5e418be0624c64fe70705671a138a70775b576
badguybandi <aaron.aminu1@gmail.com>
2024-07-10 10:46:38 +0300

    Connected frontend to backend api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import DoItTask from './components/DoItTask';
-import { getAllDoItTasks } from './utils/HandleApi';
+import { addTask, updateTask, getAllDoItTasks, deleteTask } from './utils/HandleApi';
 
 function App() {
   
   const [task, setTask] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [taskId, setTaskId] = useState("")
 
   useEffect( () => {
     getAllDoItTasks(setTask)
   }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setTaskId(_id)
+  }
 
   return (
     <div className="App">
       <div className="container">
         <h1>Do It Now</h1>
         <div className="top">
-          <input type="text" placeholder="Add task"/>
-          <div className="add">Add</div>
+          <input 
+          type="text" 
+          placeholder="Add task..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}/>
+          <div className="add" 
+          onClick={isUpdating ? 
+            () => updateTask(taskId, text, setTask,setText, setIsUpdating) 
+          : () => addTask(text, setText, setTask)}>
+            {isUpdating ? "Update" : "Add"}
+          </div>
         </div>
         <div className="list">
 
-          {task.map((item) => <DoItTask key={item._id} text={item.text}/>)}
+          {task.map((item) => <DoItTask 
+          key={item._id} 
+          text={item.text}
+          updateMode={() => updateMode(item._id, item.text)}
+          deleteTask={() => deleteTask(item._id, setTask)}/>)}
 
         </div>
       </div>
--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3002"
+const baseUrl = "http://localhost:5000"
 
 const getAllDoItTasks = (setTask) => {
     axios
@@ -11,4 +11,40 @@ const getAllDoItTasks = (setTask) => {
     })
 }
 
-export {getAllDoItTasks}
+const addTask = (text, setText, setTask) => {
+
+    axios
+    .post(`${baseUrl}/save`, {text})
+    .then((data) => {
+        console.log(data);
+        setText("")
+        getAllDoItTasks(setTask)
+    })
+    .catch((err) => console.log(err))
+}
+
+const updateTask = (taskId, text, setTask, setText, setIsUpdating) => {
+
+    axios
+    .post(`${baseUrl}/update`, {_id:taskId, text})
+    .then((data) => {
+        console.log(data);
+        setText("")
+        setIsUpdating(false)
+        getAllDoItTasks(setTask)
+    })
+    .catch((err) => console.log(err))
+}
+
+const deleteTask = (_id, setTask) => {
+
+    axios
+    .post(`${baseUrl}/delete`, {_id})
+    .then((data) => {
+        console.log(data);
+        getAllDoItTasks(setTask)
+    })
+    .catch((err) => console.log(err))
+}
+
+export {getAllDoItTasks, addTask, updateTask, deleteTask}
